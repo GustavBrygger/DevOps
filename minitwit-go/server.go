a60d1dd2dad499a21db8cd0147c9fbcd04cbc1fe
bjarkedc <bjarkeig@gmail.com>
2023-02-19 10:21:26 +0100

    added gravatar icon

--- minitwit-go/server.go
+++ minitwit-go/server.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"crypto/md5"
+	"database/sql"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
-	"github.com/noirbizarre/gonja"
-	"log"
-	"fmt"
-	"net/http"
-	"database/sql"
+	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/noirbizarre/gonja"
 	"golang.org/x/crypto/bcrypt"
-	"time"
 )
 
 var DB *sql.DB
@@ -27,43 +30,43 @@ func ConnectDatabase() error {
 }
 
 type User struct {
-	User_id         int `json:"id"`
-	Username      string `json:"username"`
+	User_id  int    `json:"id"`
+	Username string `json:"username"`
 	Email    string `json:"email"`
-	Pw_hash string `json:"pw_hash"`
+	Pw_hash  string `json:"pw_hash"`
 }
 
 type Message struct {
-	Message_id         int `json:"message_id"`
-	Author_id      int `json:"author_id"`
-	Text    string `json:"text"`
-	Pub_date int `json:"pub_date"`
-	Flagged int `json:"flagged"`
+	Message_id     int    `json:"message_id"`
+	Author_id      int    `json:"author_id"`
+	Text           string `json:"text"`
+	Pub_date       int    `json:"pub_date"`
+	Flagged        int    `json:"flagged"`
 	Formatted_date string `json:"formatted_date"`
-	Username string `json:"username"`
+	Username       string `json:"username"`
+	Gravatar       string `json:"gravatar"`
 }
 
-
-func public_timeline(c *gin.Context){
+func public_timeline(c *gin.Context) {
 	//var ids []int
 
-	messages := make([]Message, 0);
-	users := make([]User, 0);
+	messages := make([]Message, 0)
+	users := make([]User, 0)
 	session := sessions.Default(c)
 	userID := session.Get("user_id")
 
 	rows, err := DB.Query("SELECT message.*, user.* from message, user where message.flagged = 0 and message.author_id = user.user_id order by message.pub_date desc LIMIT 30")
-	
-	if err != nil{
+
+	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	defer rows.Close()
 	for rows.Next() {
 		//var _id int
 		msg := Message{}
 		usr := User{}
-		err := rows.Scan(&msg.Message_id, &msg.Author_id, &msg.Text, &msg.Pub_date, &msg.Flagged, &usr.User_id, &usr.Username, &usr.Email, &usr.Pw_hash )
+		err := rows.Scan(&msg.Message_id, &msg.Author_id, &msg.Text, &msg.Pub_date, &msg.Flagged, &usr.User_id, &usr.Username, &usr.Email, &usr.Pw_hash)
 
 		str := "Mon, 02 Jan 2006 15:04:05"
 		msg.Formatted_date = time.Unix(int64(msg.Pub_date), 0).Format(str)
@@ -72,14 +75,16 @@ func public_timeline(c *gin.Context){
 			return
 		}
 		msg.Username = usr.Username
+		msg.Gravatar = geturl(usr.Email)
+		fmt.Println(msg.Gravatar)
 
 		messages = append(messages, msg)
 		users = append(users, usr)
 	}
 
-	var tpl = gonja.Must(gonja.FromFile("template/timeline.html"))
-	
-	user, err := DB.Query("SELECT * FROM user WHERE username = ? LIMIT 1", userID)	
+	var tpl = gonja.Must(gonja.FromFile("template/timeline_go.html"))
+
+	user, err := DB.Query("SELECT * FROM user WHERE username = ? LIMIT 1", userID)
 	usr := User{}
 	user.Scan(&usr.User_id, &usr.Username, &usr.Email, &usr.Pw_hash)
 
@@ -90,27 +95,25 @@ func public_timeline(c *gin.Context){
 	}
 
 	out, err := tpl.Execute(gonja.Context{
-		"messages": messages,
-		"g": session,
-		"request": req,
+		"messages":     messages,
+		"g":            session,
+		"request":      req,
 		"profile_user": usr,
 	})
-	
+
 	fmt.Println(err)
 	c.Writer.WriteString(out)
 }
 
-
 func CheckPasswordHash(password, hash string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
 func HashPassword(password string) (string, error) {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-    return string(bytes), err
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
 }
-
 
 func login(c *gin.Context) {
 
@@ -127,7 +130,7 @@ func login(c *gin.Context) {
 		}
 
 	}*/
-	
+
 	session := sessions.Default(c)
 	if userID := session.Get("user_id"); userID != nil {
 		fmt.Println("WHAT JHJKHKJ")
@@ -135,47 +138,46 @@ func login(c *gin.Context) {
 		return
 	}
 
-	fmt.Println(session);
+	fmt.Println(session)
 	var error string
 	usr := User{}
 
 	//if c.Request.Method == http.MethodPost {
-		//username := c.PostForm("username")
-		username := "hello"
-		user, err := DB.Query("SELECT * FROM user WHERE username = ? LIMIT 1", username)
-		
-		defer user.Close()
+	//username := c.PostForm("username")
+	username := "hello"
+	user, err := DB.Query("SELECT * FROM user WHERE username = ? LIMIT 1", username)
 
-		for user.Next() {
-			user.Scan(&usr.User_id, &usr.Username, &usr.Email, &usr.Pw_hash)
-			break
-		}
-		
-		fmt.Println(usr)
-		if err != nil{
-			log.Fatal(err);
-		}
-		if user == nil {
-			fmt.Println("Invalid username");
-		} else if !CheckPasswordHash("secret", usr.Pw_hash) {
-			fmt.Println("Invalid password");
-		} else {
-			session.Set("flash", "You were logged in")
-			session.Set("user_id", usr.User_id)
-			session.Save()
-			fmt.Println(":)")
-			c.Redirect(http.StatusFound, "/public")
-			return
-		}
+	defer user.Close()
+
+	for user.Next() {
+		user.Scan(&usr.User_id, &usr.Username, &usr.Email, &usr.Pw_hash)
+		break
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	if user == nil {
+		fmt.Println("Invalid username")
+	} else if !CheckPasswordHash("secret", usr.Pw_hash) {
+		fmt.Println("Invalid password")
+	} else {
+		session.Set("flash", "You were logged in")
+		session.Set("user_id", usr.User_id)
+		session.Save()
+		fmt.Println(":)")
+		c.Redirect(http.StatusFound, "/public")
+		return
+	}
 	//}
 
-	c.JSON(http.StatusOK, gin.H{"error":error, "user": usr});
+	c.JSON(http.StatusOK, gin.H{"error": error, "user": usr})
 	//c.HTML(http.StatusOK, "login.html", gin.H{"error": error})
 }
 
-func register(c *gin.Context){
+func register(c *gin.Context) {
 	password := "secret"
-    hash, _ := HashPassword(password)
+	hash, _ := HashPassword(password)
 	email := "hellohellocom"
 	username := "hello"
 
@@ -204,12 +206,21 @@ func register(c *gin.Context){
 
 }
 
+func geturl(email string) string {
+	email_md5 := fmt.Sprintf("%s", md5.Sum([]byte(email)))
+	hex_md5_email := hex.EncodeToString([]byte(email_md5))
+
+	//format string
+	url := fmt.Sprintf("https://www.gravatar.com/avatar/%s?d=identicon&s=48", hex_md5_email)
+	return url
+}
+
 func main() {
 	r := gin.Default()
 	store := cookie.NewStore([]byte("secret"))
 	r.Use(sessions.Sessions("mysession", store))
 
-	r.LoadHTMLFiles("./template/timeline.html")
+	r.LoadHTMLFiles("./template/timeline_go.html")
 	r.Static("/static", "./static/")
 
 	ConnectDatabase()
@@ -221,4 +232,3 @@ func main() {
 
 	r.Run()
 }
-
