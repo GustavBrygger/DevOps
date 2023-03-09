ba07bcb644bf9d0a4fb7d3293ab020f81ad7b6c9
Oliver Jarvis <40406126+oliverjarvis@users.noreply.github.com>
2023-03-09 15:31:21 +0100

    Update seed.go

--- src/persistence/seed.go
+++ src/persistence/seed.go
@@ -16,7 +16,7 @@ func seed(db *gorm.DB) {
 }
 
 func addUsersAndMessages(db *gorm.DB) {
-	db.Exec("INSERT INTO users (id, username, email, pw_hash) VALUES (0, "Roger Histand", "Roger+Histand@hotmail.com", "$2a$14$o0RLqWDxXvMAvcTrKqYVbuF4JcTT5i8tS2b8nzSN4u.UP5odFsFdG");")
+	db.Exec("INSERT INTO users (id, username, email, pw_hash) VALUES (0, 'Roger Histand', 'Roger+Histand@hotmail.com', '$2a$14$o0RLqWDxXvMAvcTrKqYVbuF4JcTT5i8tS2b8nzSN4u.UP5odFsFdG');")
 
 	user1 := application.User{
 		Username: "AndenTester",
