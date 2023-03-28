7d4dc8c679e075cde5d64a4522d04a9381cc476c
Oliver Jarvis <oliversimonjarvis@gmail.com>
2023-03-28 15:54:24 +0200

    befr＠

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -6,4 +6,5 @@ docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml up -d
 docker-compose restart prometheus
 docker-compose restart grafana
+
 docker pull $DOCKER_USERNAME/flagtoolimage:latest
