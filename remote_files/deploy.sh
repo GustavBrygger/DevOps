37a312fafa9aced96b507ff08ae7ca45db1d3c1a
Oliver Jarvis <oliversimonjarvis@gmail.com>
2023-03-28 14:55:10 +0200

    updated deploy

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -5,4 +5,5 @@ cd /minitwit
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml up -d
 docker-compose restart prometheus
+docker-compose restart grafana
 docker pull $DOCKER_USERNAME/flagtoolimage:latest
