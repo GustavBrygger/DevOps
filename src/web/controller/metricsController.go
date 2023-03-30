9779640c5d883f5f41b7c0f148b6841a57ca92fc
Oliver Jarvis <oliversimonjarvis@gmail.com>
2023-03-30 16:45:40 +0200

    fixed prometheus endpoints

--- src/web/controller/metricsController.go
+++ src/web/controller/metricsController.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/shirou/gopsutil/v3/mem",
+	"github.com/shirou/gopsutil/v3/mem"
 	"string"
 )
 
