5c0a31fbf47d041ae01ee56b19dacf8302998632
Niklas Brynfeldt <niklasbg@live.dk>
2023-04-18 14:05:20 +0200

    Fixed nginx.conf path in DO docker-compose

--- setup-elk.sh
+++ setup-elk.sh
@@ -1,5 +1,5 @@
 printf "temp:$(openssl passwd -crypt TEMP)\n" > .htpasswd
 
-# Change permissions on filebat config
+# Change permissions on filebeat config
 sudo chown root filebeat.yml 
 sudo chmod go-w filebeat.yml
