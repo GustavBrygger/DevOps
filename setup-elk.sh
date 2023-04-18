4713e68449cdf004343202cdcaa84a876a9d158a
Niklas Brynfeldt <niklasbg@live.dk>
2023-04-18 15:03:33 +0200

    Limit Elastichsearch memory usage

--- setup-elk.sh
+++ setup-elk.sh
@@ -1,5 +1,5 @@
 printf "temp:$(openssl passwd -crypt temp)\n" > .htpasswd
 
 # Change permissions on filebeat config
-sudo chown root filebeat.yml 
-sudo chmod go-w filebeat.yml
+sudo chown root ./remote_files/filebeat.yml 
+sudo chmod go-w ./remote_files/filebeat.yml
