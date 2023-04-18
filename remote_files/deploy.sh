aa259d65ec386a7dce841684001a9b9188bd174c
Niklas Brynfeldt <niklasbg@live.dk>
2023-04-18 13:46:44 +0200

    Configure ELK in deploy script in production

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -2,6 +2,13 @@ source ~/.bash_profile
 
 cd /minitwit
 
+# Configure ELK stack
+printf "temp:$(openssl passwd -crypt TEMP)\n" > .htpasswd
+
+# Change permissions on filebat config
+sudo chown root filebeat.yml 
+sudo chmod go-w filebeat.yml
+
 docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml up -d
 docker-compose restart prometheus
